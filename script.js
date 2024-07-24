8c7320e5e79588d6d3af32e3483544d11a3e0dcc
Gowynn Hunt <gowynnhunt@gmail.com>
2024-07-24 11:56:06 -0500

    Add display functionality based on array
    My plan is to parse the array for a series of operations to complete.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
+function operate(a, operator, b) {
   switch (operator) {
     case "+":
       return add(a, b);
@@ -28,3 +28,45 @@ function operate(operator, a, b) {
       return "Doh!";
   }
 }
+
+// Regex matching for numbers
+function isNum(str) {
+  return str.match(/[0-9]/);
+}
+
+// Regex matching for operators
+function isOperator(str) {
+  return str.match(/[+\-*/]/);
+}
+
+function buttonHandler(event) {
+  let id = event.target.id;
+
+  if (isNum(id)) {
+    if (calcArr[idx] === undefined || calcArr[idx] === "0") {
+      calcArr[idx] = id;
+    } else if (isOperator(calcArr[idx])) {
+      idx++;
+      calcArr[idx] = id;
+    } else {
+      calcArr[idx] += id;
+    }
+  } else if (isOperator(id)) {
+    if (isNum(calcArr[idx])) idx++;
+    calcArr[idx] = id;
+  } else if (id === "clear") {
+    // Matches clear
+    calcArr = ["0"];
+    idx = 0;
+  }
+  display.textContent = calcArr.join(" ");
+  console.log(calcArr);
+}
+
+let calcArr = [];
+let idx = 0;
+
+const display = document.querySelector("#display");
+
+const buttons = document.querySelectorAll("#calculator button");
+buttons.forEach((button) => button.addEventListener("click", buttonHandler));
