616da55f969c3207169341fc2cd9cd04719297be
Gowynn Hunt <gowynnhunt@gmail.com>
2024-07-24 17:09:32 -0500

    Add calculation feature
    I have succefully added the calculation feature I had in mind. TOP
    provides a student example that has the same functionality as mine, but
    it appears to be a little more simple. It won't allow you to fully input
    a long mathematical expression that would be incorrect.
    
    I wrote an example into the comments of my code that would produce an
    incorrect value because it evaluates the code from left to right without
    respect to traditional order of operations. After thinking about this,
    I'm going to open a new branch in my code to rectify that and make it so
    that only single operations are shown at once.

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(a, operator, b) {
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
   switch (operator) {
     case "+":
       return add(a, b);
@@ -58,9 +60,48 @@ function buttonHandler(event) {
     // Matches clear
     calcArr = ["0"];
     idx = 0;
+  } else if (id === "=") {
+    calculate(calcArr);
   }
   display.textContent = calcArr.join(" ");
   console.log(calcArr);
+}
+
+// Calculates by taking slices of calcArr. This way, the user can input
+// as many operation as they want at once before choosing to calculate.
+
+// It's worth mentioning that this won't properly calculate the value of
+// more complex interactions.
+//
+// Example: 12 - 7 + 6 * 3 = 23, but my calculator will return 33
+// because it strictly solves from left to right.
+function calculate(arr) {
+  // Guard against operation without enough operands
+  if (arr.length < 3 || arr.length % 2 === 0) return;
+
+  // Stage initial values
+  let slice = [];
+  let total = 0;
+
+  //
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    if (i === 0) {
+      // Initial slice
+      slice = arr.slice(0, 3);
+      total = operate(slice[1], slice[0], slice[2]);
+    } else {
+      // Every slice after the first will grab the next operation and
+      // operand and use the previous total as its first value
+      slice = arr.slice(i * 2 + 1, i * 2 + 3);
+      total = operate(slice[0], total, slice[1]);
+    }
+  }
+
+  // Needs string to display
+  calcArr = [total.toString()];
+
+  // Resetting idx for further operation
+  idx = 0;
 }
 
 let calcArr = [];
